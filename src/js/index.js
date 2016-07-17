423ebb5becd91a184bdfe6b614fb9e99cc81c21b
Ro_OLeary <roleary81@gmail.com>
2016-07-17 19:04:39 +0100

    Commit updates to origin

--- src/js/index.js
+++ src/js/index.js
@@ -12,6 +12,8 @@ jQuery(document).ready(function(){
       cache: false
     } );
   } );
+
+
 } );
 
 
@@ -28,7 +30,7 @@ jQuery.ajax({
         jQuery.each(data, function (index, data) {
             //console.log(data);
             jQuery('ul').append(
-              jQuery('<li><img src='+ data.poster + ' />').append(
+              jQuery('<li data-filter="'+ data.i + '"><img src='+ data.poster + ' />').append(
                 jQuery('<article>').append(
                     jQuery('<h5 class="title">' + data.title +'<span class="year">('+ data.year +')</span></h5><small class="genre">Genre: '+ data.genre +'</small><small class="type">Genre: '+ data.type +'</small>').append()
         
@@ -37,4 +39,37 @@ jQuery.ajax({
     })
   }});
 
+jQuery(document).ready(function(){
+
+  var options = {
+  animationDuration: 0.5, //in seconds
+    filter: 'all', //Initial filter
+      callbacks: { 
+        onFilteringStart: function() { },
+        onFilteringEnd: function() { },
+        onShufflingStart: function() { },
+        onShufflingEnd: function() { },
+        onSortingStart: function() { },
+        onSortingEnd: function() { }
+      },
+      delay: 0, //Transition delay in ms
+      delayMode: 'progressive', //'progressive' or 'alternate'
+      easing: 'ease-out',
+      filterOutCss: { //Filtering out animation
+        opacity: 0,
+        transform: 'scale(0.5)'
+        },
+      filterInCss: { //Filtering in animation
+        opacity: 0,
+        transform: 'scale(1)'
+      },
+      layout: 'sameSize', //See layouts
+      selector: '.filtr-container',
+      setupControls: true 
+  } 
+  //You can override any of these options and then call...
+  var filterizd = jQuery('.filtr-container').filterizr(options);
+  //If you have already instantiated your Filterizr then call...
+  filterizd.filterizr('setOptions', options);
+});
 
