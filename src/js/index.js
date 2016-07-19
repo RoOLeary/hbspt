5839caab1aef5ada4c8e09b1179fa5c99ad906d7
Ro_OLeary <roleary81@gmail.com>
2016-07-19 20:04:30 +0100

    remove crappy script and its likeness FOREVER! - tidy index.js"

--- src/js/index.js
+++ src/js/index.js
@@ -6,35 +6,12 @@ jQuery(document).ready(function(){
     jQuery.ajax( {
       // Grab a random joke from the Chuck Norris APO
       url: 'https://api.chucknorris.io/jokes/random',
-	  success: function ( data ) {
-       jQuery( '.cta-left' ).text( data.value );
+	       success: function ( data ) {
+          jQuery( '.cta-left' ).text( data.value );
       },
       cache: false
     } );
   } );
 } );
-
-
-// Calling in the data from data.json, assigning to temporary objects
-
-// jQuery.ajax({
-//   type: 'GET', 
-//     url: "../src/js/data/data.json",
-//     dataType: "json",
-//     success: function (data) {
-//       //console.log(data);
-//       jQuery.each(data, function (key, data) {
-//         //console.log(key); 
-//         jQuery.each(data, function (index, data) {
-//             //console.log(data);
-//             jQuery('ul').append(
-//               jQuery('<li><img src='+ data.poster + ' />').append(
-//                 jQuery('<article>').append(
-//                     jQuery('<h5 class="title">' + data.title +'<span class="year">('+ data.year +')</span></h5><small class="genre">Genre: '+ data.genre +'</small>').append()
-        
-//             )));
-//         }); 
-//     })
-//   }});
 
 
