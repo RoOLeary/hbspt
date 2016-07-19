37cf59c6064a916e3c3e2834bdc3bf55f2590f6e
Ro_OLeary <roleary81@gmail.com>
2016-07-19 12:22:57 +0100

    Commit npm react as opposed to fb.me link

--- build/js/index.js
+++ build/js/index.js
@@ -18,24 +18,24 @@ jQuery(document).ready(function(){
 
 // Calling in the data from data.json, assigning to temporary objects
 
-jQuery.ajax({
-  type: 'GET', 
-    url: "../src/js/data/data.json",
-    dataType: "json",
-    success: function (data) {
-      //console.log(data);
-      jQuery.each(data, function (key, data) {
-        //console.log(key); 
-        jQuery.each(data, function (index, data) {
-            //console.log(data);
-            jQuery('ul').append(
-              jQuery('<li><img src='+ data.poster + ' />').append(
-                jQuery('<article>').append(
-                    jQuery('<h5 class="title">' + data.title +'<span class="year">('+ data.year +')</span></h5><small class="genre">Genre: '+ data.genre +'</small>').append()
+// jQuery.ajax({
+//   type: 'GET', 
+//     url: "../src/js/data/data.json",
+//     dataType: "json",
+//     success: function (data) {
+//       //console.log(data);
+//       jQuery.each(data, function (key, data) {
+//         //console.log(key); 
+//         jQuery.each(data, function (index, data) {
+//             //console.log(data);
+//             jQuery('ul').append(
+//               jQuery('<li><img src='+ data.poster + ' />').append(
+//                 jQuery('<article>').append(
+//                     jQuery('<h5 class="title">' + data.title +'<span class="year">('+ data.year +')</span></h5><small class="genre">Genre: '+ data.genre +'</small>').append()
         
-            )));
-        }); 
-    })
-  }});
+//             )));
+//         }); 
+//     })
+//   }});
 
 
